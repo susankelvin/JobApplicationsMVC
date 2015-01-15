dd72591517853b5343e81c5f3042748e27a40764
susankelvin <susankelvin@users.noreply.github.com>
2015-01-15 21:01:51 +0200

    Initial commit.

---
+++ Database/Data/JobApplications.Database.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace JobApplications.Database.Data
+{
+    using JobApplications.Database.Models;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Microsoft.AspNet.Identity;
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ Database/Models/JobApplications.Database.Models/Application.cs
@@ -0,0 +1,27 @@
+﻿namespace JobApplications.Database.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Application
+    {
+        public int  Applicationid { get; set; }
+
+        [Required]
+        public string AuthorId { get; set; }
+
+        public ApplicationUser Author { get; set; }
+
+        [Required]
+        public string Position { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+
+    }
+}
