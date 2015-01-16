0daa436734f63280be1e9ee5313d7c6281a09c48
susankelvin <susankelvin@users.noreply.github.com>
2015-01-16 13:48:18 +0200

    Bootstrap theme United.

--- Web/JobApplications.Web/App_Start/BundleConfig.cs
+++ Web/JobApplications.Web/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace JobApplications.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.united.css",
                       "~/Content/site.css"));
         }
     }
