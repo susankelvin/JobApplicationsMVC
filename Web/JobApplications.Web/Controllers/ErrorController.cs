0ea97f506f9271f7fba8c340621b5c500bbea638
susankelvin <susankelvin@users.noreply.github.com>
2015-01-22 02:37:12 +0200

    Error views and logic.

---
+++ Web/JobApplications.Web/Controllers/ErrorController.cs
@@ -0,0 +1,48 @@
+﻿namespace JobApplications.Web.Controllers
+{
+    using System.Web.Mvc;
+    using JobApplications.Database.Data.Interfaces;
+    using JobApplications.Web.Controllers.Base;
+    using JobApplications.Web.Models.Error;
+
+    public class ErrorController : BaseController
+    {
+        public ErrorController(IData data)
+            : base(data)
+        {
+        }
+
+        // GET: Error
+        public ActionResult Index()
+        {
+            ErrorViewModel model = new ErrorViewModel()
+            {
+                PanelHeading = "We are sorry",
+                PanelBody = "We are not able to process Your request right now. Please, try again later."
+            };
+            return View("Error", model);
+        }
+
+        // GET: NotFound
+        public ActionResult NotFound()
+        {
+            ErrorViewModel model = new ErrorViewModel()
+            {
+                PanelHeading = "Not found",
+                PanelBody = "The requested page was not found. Check the address and try again."
+            };
+            return View("NotFound", model);
+        }
+
+        // GET: BadRequest
+        public ActionResult BadRequest()
+        {
+            ErrorViewModel model = new ErrorViewModel()
+            {
+                PanelHeading = "Bad Request",
+                PanelBody = "We are not able to understand Your request."
+            };
+            return View("BadRequest", model);
+        }
+    }
+}
---
+++ Web/JobApplications.Web/Models/Error/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JobApplications.Web.Models.Error
+{
+    public class ErrorViewModel
+    {
+        public string PanelHeading { get; set; }
+
+        public string PanelBody { get; set; }
+    }
+}
